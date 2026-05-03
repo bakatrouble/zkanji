d005340c3b9bd25f51d3a04b25acb2f8b65dae04
bakatrouble <bakatrouble@gmail.com>
2026-05-03 19:45:48 +0300

    add import files download script

---
+++ dataimports.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://ftp.edrdg.org/pub/Nihongo/JMdict.gz -O - | gzip -d > dataimports/JMdict
+wget http://www.edrdg.org/kanjidic/kanjidic.gz -O - |  gzip -d > dataimports/kanjidic
+wget http://ftp.edrdg.org/pub/Nihongo/radkfile.gz -O - | gzip -d > dataimports/radkfile
